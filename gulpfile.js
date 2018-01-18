cd715801d0b73395c81597652f935d7b460711a0
tranki1 <kimhieutran100@gmail.com>
2018-01-18 12:49:11 +0200

    setting up gulp,content and presentation for css variables project

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,14 @@
 var gulp = require("gulp"),
-  gutil = require("gulp-util"),
   connect = require("gulp-connect");
-watch = require("gulp-watch");
 
-var jsSources, outputDir;
-outputDir = "clock/";
-
-jsSources = ["*.js"];
+var outputDir;
+outputDir = "PlayingWithCSSVariablesAndJS/";
 
 gulp.task("js", function() {
   gulp
-    .src(outputDir + "js/*.js")
+    .src(outputDir + "js/**")
     .pipe(gulp.dest(outputDir + "js"))
     .pipe(connect.reload());
-});
-
-gulp.task("watch", function() {
-  gulp.watch(outputDir + "js/*.js", ["js"]);
-  gulp.watch(outputDir + "*.html", ["html"]);
-  gulp.watch(outputDir + "css/*.css", ["css"]);
-});
-
-gulp.task("connect", function() {
-  connect.server({
-    root: outputDir,
-    livereload: true
-  });
 });
 
 gulp.task("html", function() {
@@ -36,9 +19,22 @@ gulp.task("html", function() {
 });
 gulp.task("css", function() {
   gulp
-    .src(outputDir + "css/*.css")
+    .src(outputDir + "css/**")
     .pipe(gulp.dest(outputDir + "css"))
     .pipe(connect.reload());
+});
+
+gulp.task("watch", function() {
+  gulp.watch(outputDir + "js/**", ["js"]);
+  gulp.watch(outputDir + "*.html", ["html"]);
+  gulp.watch(outputDir + "css/**", ["css"]);
+});
+
+gulp.task("connect", function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
 });
 
 gulp.task("default", ["html", "js", "css", "connect", "watch"]);
