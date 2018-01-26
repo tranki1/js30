6b9f6d6cc5a30073c696ead2b2c54aa3f584ad26
tranki1 <kimhieutran100@gmail.com>
2018-01-26 23:13:17 +0200

    add slide in on scroll function

--- Slide-in-on-Scroll/js/script.js
+++ Slide-in-on-Scroll/js/script.js
@@ -13,3 +13,23 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e) {
+  sliderImages.forEach(img => {
+    //haft way through the image
+    const slideInAt = (window, scrollY + window.innerHeight) + img.height / 2;
+    //the bottom of the image
+    const imageBottom = img.offsetTop + img.height;
+    const isHaft = slideInAt > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHaft && isNotScrolledPast) {
+      img.classList.add("active");
+    } else {
+      img.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide, 5));
